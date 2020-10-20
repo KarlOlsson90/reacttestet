9dad1659f856796073ebcc21a046c72b70d3c50b
Karl Olsson <karl.olsson.mail@gmail.com>
2020-10-20 15:02:18 +0200

    nytt forsok

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,40 +1,24 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import PostsItem from './components/PostsItem';
 
 function App() {
-
-  const [postsData, setPostsData] = useState([])
-  const [expandedItem, setExpandedItem] = useState(null)
-  const [likedItem, setLikedItem] = useState(null)
-  
-  function fetchPosts() {
-    fetch("https://image-mock-data.firebaseio.com/images.json")
-    .then(res => res.json())
-    .then(data => {
-      setPostsData(data)
-    })
-  }
-
-  useEffect( () => {
-    fetchPosts()
-  }, [] )
-
   return (
-    <div className="container">
-      {postsData.map((postsItem, index) => {
-        return <PostsItem 
-              key={index} 
-              index={index}
-              expandedItem ={expandedItem} 
-              setExpandedItem={setExpandedItem}
-              postContent={postsItem}
-              //likedItem={likedItem}
-              //setLikedItem={setLikedItem}
-              
-              />
-      })}
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
